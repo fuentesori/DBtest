3f8376c68e1dabecb4569268a516742efe809d7f
Oriana I. Fuentes <Oriana I. Fuentes>
2016-11-09 14:20:11 -0500

    new database skeleton based on tutorial vid

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import request, redirect, url_for, render_template
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ok@localhost/dbtest'
+app.debug = True
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(9))
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' %self.username
+
+@app.route('/')
+def index():
+    myUser = User.query.all()
+    oneItem = User.query.filter_by(username="test2").all()
+    return render_template('add_user.html', myUser = myUser, oneItem = oneItem)
+
+@app.route('/profile/<username>')
+def profile(username):
+    user = User.query.filter_by(username=username).first()
+    return render_template('profile.html', user=user)
+
+@app.route('/portfolio/<username>')
+def profile(username):
+    user = User.query.filter_by(username=username).first()
+    return render_template('profile.html', user=user)
+
+
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    user = User(request.form['username'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__=="__main__":
+    app.run()
