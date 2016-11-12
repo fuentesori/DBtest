6bda43e72f3e1e8e6623c382d1e6b25bbc888814
Oriana I. Fuentes <Oriana I. Fuentes>
2016-11-12 13:08:24 -0500

    tweaked navigation

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
+import datetime
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, redirect, url_for, render_template, g, Response
+
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -44,42 +46,47 @@ def index():
 
 @app.route('/check_login', methods=['POST'])
 def checklogin():
-    link = 'badlog'
-    uid = request.form['uid']
+    #set up variables
+    link = []
+    loginuid = request.form['uid']
     checkuser = null
+    #SQL query the user login
     cmd ="SELECT uid from users where uid=%s"
-    cursor = g.conn.execute(cmd,uid)
+    cursor = g.conn.execute(cmd,loginuid[0])
     for result in cursor:
         checkuser = result
-        print(type(uid),type(checkuser[0]))
-        if checkuser[0] == float(uid):
-            link = 'portfolio'
-    return redirect(url_for(link, uid=uid))
 
-@app.route('/incorrectlogon/<uid>')
-def badlog(uid):
-    return render_template("incorrectlogon.html", uid=uid)
+        #check if loginuid is good in database
+        if int(checkuser[0]) == int(loginuid):
+            link = 'portfolio'
+            #set global uid if uid is good
+            global uid
+            uid = int(checkuser[0])
+        else:
+            link = 'badlog'
+    return redirect(url_for(link, passuid=int(loginuid), portfolioid=0))
+
+@app.route('/incorrectlogon/<passuid>/<portfolioid>')
+def badlog(passuid, portfolioid):
+    return render_template("incorrectlogon.html", baduid=passuid)
 
 #User portfolio
+@app.route('/portfolio/<passuid>/<portfolioid>', methods=['POST'])
+def populateportfolio(passuid,portfolioid):
+    global Gportfolioid
+    Gportfolioid = float(request.form['portfolio'])
+    return redirect(url_for('portfolio', passuid=uid, portfolioid=Gportfolioid))
 
-@app.route('/portfolio/<uid>/<portfolioid>', methods=['POST'])
-def populateportfolio(uid,portfolioid):
-    print("at leat i got here")
-    uid = 1
-    #portfolioid=[]
-    #portfolioid = request.form['portfolio']
-    return redirect(url_for('portfolio', uid=uid))
-
-@app.route('/portfolio/<uid>')
-def portfolio(uid):
+@app.route('/portfolio/<passuid>/<portfolioid>')
+def portfolio(passuid, portfolioid):
+    #obtain user's list of portfolios
     cmd ="SELECT portfolioid FROM portfolio where uid=%s"
     cursor = g.conn.execute(cmd, uid)
     portfolios = []
     for result in cursor:
         portfolios.append(result[0])
     cursor.close()
-    portfolioid=9
-
+    #render user's portfoliodata
     cmd ="SELECT * FROM stock_transactions where uid=%s and portfolioid=%s"
     cursor = g.conn.execute(cmd, uid, portfolioid)
     transactions = []
@@ -100,14 +107,14 @@ def portfolio(uid):
         tickers.append(result)
     cursor.close()
 
-    cmd = "SELECT accountid FROM bank_accounts WHERE uid=%s ORDER BY accountid"
+    cmd = "SELECT bankaccountid FROM bank_accounts WHERE uid=%s ORDER BY bankaccountid"
     cursor = g.conn.execute(cmd,uid)
-    accounts = []
+    bankaccountids = []
     for result in cursor:
-        accounts.append(result[0])
+        bankaccountids.append(result[0])
     cursor.close()
 
-    return render_template("portfolio.html", portfolios=portfolios, transactions=transactions, user=userinfo, tickers=tickers, accounts=accounts)
+    return render_template("portfolio.html", portfolios=portfolios, transactions=transactions, user=userinfo, tickers=tickers, bankaccountids=bankaccountids)
 
 @app.route('/post_trade', methods=['POST'])
 def post_trade():
@@ -116,18 +123,29 @@ def post_trade():
     g.conn.execute(cmd, (users[0], users[1], users[2], users[3], users[4], users[5], users[6], users[7], users[8], users[9], users[10]));
     return redirect(url_for('portfolio', uid=uid))
 
-    # @app.route('/post_cash', methods=['POST'])
-    # def post_user():
-    #     uid=1
-    #     portfolioid =9
-    #     cmd = 'SELECT MAX(transactionid) FROM cash_transactions'
-    #     cursor = g.conn.execute(cmd)
-    #     for result in cursor:
-    #         transactionid = cursor + 1
-    #     cash = [transactionid, request.form['uid'], request.form['fname'], request.form['lname'], request.form['address'], request.form['phone'], request.form['ssn']]
-    #     cmd = 'INSERT INTO cash_transactions VALUES (%s, %s, %s, %s, %s, %s)';
-    #     g.conn.execute(cmd, (cash[0], cash[1], cash[2], cash[3], users[4], users[5]));
-    #     return redirect(url_for('portfolio', uid=uid))
+@app.route('/post_cash', methods=['POST'])
+def post_cash():
+    amount=[]
+    tdate = ""
+    #find biggest primary key id and increment by 1
+    cmd = 'SELECT MAX(transactionid) FROM cash_transactions'
+    cursor = g.conn.execute(cmd)
+    for result in cursor:
+        transactionid = result[0] + 1
+    #set transfer data
+    tdate = datetime.date.today()
+    tdate = str(tdate)
+    #determine whether amount is positive or negative
+    if request.form['order']=='In':
+        amount = float(request.form['amount'])
+    else:
+        amount = float(request.form['amount'])*-1
+    #insert transaction into database
+    cash = [transactionid, tdate, uid, float(request.form['bankaccountid']), Gportfolioid, amount]
+    cmd = 'INSERT INTO cash_transactions VALUES (%s, %s, %s, %s, %s, %s)';
+    g.conn.execute(cmd, (cash[0], cash[1], cash[2], cash[3], cash[4], cash[5]));
+    return redirect(url_for('portfolio', passuid=uid, portfolioid=Gportfolioid))
+
 
 
 #New User page and adding new user to database
@@ -135,23 +153,36 @@ def post_trade():
 def newuser():
     return render_template("add_user.html")
 
+@app.route('/usercreated/<uid>')
+def usercreated(uid):
+    return render_template("usercreated.html", uid=uid)
+
 @app.route('/post_user', methods=['POST'])
 def post_user():
-    users = [request.form['uid'], request.form['fname'], request.form['lname'], request.form['address'], request.form['phone'], request.form['ssn']]
+    #find biggest primary key id and increment by 1
+    cmd = 'SELECT MAX(uid) FROM users'
+    cursor = g.conn.execute(cmd)
+    for result in cursor:
+        uid = result[0] + 1
+    #fill out user data and store in database
+    users = [uid, request.form['fname'], request.form['lname'], request.form['address'], request.form['phone'], request.form['ssn']]
     cmd = 'INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s)';
     g.conn.execute(cmd, (users[0], users[1], users[2], users[3], users[4], users[5]));
-    return redirect(url_for('newuser'))
+    return redirect(url_for('usercreated', uid=uid))
 
 #Existing user profile, view and update user data
 @app.route('/profile')
 def profile():
-    cursor = g.conn.execute("SELECT * FROM users where uid=1")
-    names = []
+    cmd = "SELECT * FROM users where uid=%s"
+    cursor = g.conn.execute(cmd,uid)
+    userdata = []
     for result in cursor:
-        names=result
+        userdata=result
     cursor.close()
-    return render_template("profile.html", names=names)
+    return render_template("profile.html", userdata=userdata)
 
+#@app.route('/profile/successfulupdate')
+#def update_user():
 
 
 
