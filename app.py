afbe115588c944db70fbbf324bf6ebef7b573f28
Oriana I. Fuentes <Oriana I. Fuentes>
2016-11-14 07:45:44 -0500

    changed server

--- app.py
+++ app.py
@@ -488,10 +488,10 @@ if __name__ == "__main__":
   @click.command()
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='localhost')
-  @click.argument('PORT', default=8081, type=int)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
   def run(debug, threaded, host, port):
-    """
+          """
     This function handles command line parameters.
     Run the server using
         python server.py
