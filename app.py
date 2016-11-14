8f00c339fc8fd725cd63435b3179e9214daed7cc
Oriana I. Fuentes <Oriana I. Fuentes>
2016-11-14 08:45:29 -0500

    server

--- app.py
+++ app.py
@@ -488,8 +488,8 @@ if __name__ == "__main__":
   @click.command()
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='localhost')
-  @click.argument('PORT', default=8111, type=int)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8081, type=int)
   def run(debug, threaded, host, port):
     """
     This function handles command line parameters.
