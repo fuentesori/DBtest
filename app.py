1d357edce8160987b9331ad6210478edf324aae7
Oriana I. Fuentes <Oriana I. Fuentes>
2016-11-14 08:38:50 -0500

    fixed formatting of tables a little bit

--- app.py
+++ app.py
@@ -488,7 +488,7 @@ if __name__ == "__main__":
   @click.command()
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('HOST', default='localhost')
   @click.argument('PORT', default=8111, type=int)
   def run(debug, threaded, host, port):
     """
@@ -500,7 +500,7 @@ if __name__ == "__main__":
     """
 
     HOST, PORT = host, port
-    print "running on %s:%d" % (HOST, PORT)
+    #print("running on" %s:%d) % (HOST, PORT)
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 
